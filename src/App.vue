d61289f16d12eda88ddc3e767323ac12221d2e01
Kenny <kenny.reyes>
2021-06-21 02:55:22 +0200

    save

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,56 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld :value.sync="lessonItem.text" :label="label" :rules="rule.text" />
+    <TextBox :value.sync="lessonItem.text" :label="label" :rules="rule.text" />
+    <span>{{ lessonItem.text }}</span>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/RichText.vue";
+import TextBox from "./components/TextBox";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    TextBox,
+    HelloWorld,
+  },
+  data() {
+    return {
+      lessonItem: {
+        id: 0,
+        code: '',
+        name: '',
+        description: '',
+        text: '',
+        difficulty: '',
+        order: 0,
+        topicId: 0,
+        programId: 0,
+        duration: '00:10:00',
+      },
+      label: 'Hola'
+    };
+  },
+  computed: {
+    rule() {
+      return {
+        text: [(v) => !!v || 'common.rules.required']
+      };
+    },
+  },
+  watch: {
+    lesson: {
+      handler: function (lesson) {
+        let lessonAux;
+        lessonAux.form.text = "Hola"
+        this.lessonItem = Object.assign({ }, lesson);
+      },
+    },
+  },
+
+};
 </script>
 
 <style>
---
+++ src/components/TextBox.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-textarea v-model="localValue" :label="label" :height="height" :rules="rules" dense outlined />
+</template>
+
+<script>
+import vmodelMixin from '@/mixins/vmodel.mixin';
+
+export default {
+  mixins: [vmodelMixin],
+  props: {
+    label: {
+      type: String,
+      default: '',
+    },
+    height: {
+      type: String,
+      default: '',
+    },
+    rules: {
+      type: Array,
+      default: function () {
+        return [];
+      },
+    },
+  },
+  data() {
+    return {
+    };
+  },
+};
+</script>
